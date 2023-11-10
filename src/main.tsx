72feaa59962a7af7cfcdf81665b8fb731fa9051a
LizavetaZ <vokibzoj@yandex.by>
2023-11-11 00:54:15 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import * as React from 'react'
 
 import { createRoot } from 'react-dom/client'
 
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import { App } from './App'
 
 // import React = require('react')
